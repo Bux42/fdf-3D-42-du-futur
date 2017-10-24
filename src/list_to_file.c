45f6bf94993c5e7357ec73c0c293ae32063c2407
Virgile DESVAUX <videsvau@e1r9p14.42.fr>
2017-10-24 12:55:01 +0200

    pr sa xddd

--- src/list_to_file.c
+++ src/list_to_file.c
@@ -6,7 +6,7 @@
 /*   By: videsvau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/24 12:49:09 by videsvau          #+#    #+#             */
-/*   Updated: 2017/10/24 12:52:52 by videsvau         ###   ########.fr       */
+/*   Updated: 2017/10/24 12:54:26 by videsvau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void		list_to_file(t_ins **ins)
 	cp = (*ins);
 	while (cp)
 	{
+		if (cp->start)
+			ft_putchar('\n');
 		ft_putchar('[');
 		ft_putnbr(cp->z);
 		ft_putchar(',');
